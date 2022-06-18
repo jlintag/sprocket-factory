df8f26de4a890a86492f9f319e544beb6aaff1c7
Josh Lintag <jlintag@gmail.com>
2022-06-18 18:27:01 -0400

    fix factory structure

--- src/api/main.py
+++ src/api/main.py
@@ -18,7 +18,18 @@ app = FastAPI()
 app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"])
 
 
+def sanitize_factory_object(factory: Factory):
+    return {
+        "chart_data": {
+            "sprocket_production_actual": factory.sprocket_production_actual,
+            "sprocket_production_goal": factory.sprocket_production_goal,
+            "time": factory.time,
+        },
+        "id": factory.id,
+    }
+
+
 @app.get("/factories")
 def get_factories():
     factories = db.session.query(ModelFactory).all()
-    return factories
+    return [sanitize_factory_object(var) for var in factories]
